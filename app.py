dda8148041bd0bfff254c48981f71fff5078a673
Anukul Anand <anukul.anand2000@gmail.com>
2021-05-09 23:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/helloesp', methods = ['GET'])
 def helloHandler():
     return 'Hello ESP8266, from Flask'
+
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
  
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
