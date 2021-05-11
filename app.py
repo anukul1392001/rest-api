b1d1d2d8067184e0adfb0c557f9402518414ecbe
Anukul Anand <anukul.anand2000@gmail.com>
2021-05-11 05:47:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-# app.py
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/helloesp', methods = ['GET'])
-def helloHandler():
-    response =  'Hello ESP8266, from Flask'
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+    
 
- 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+   app.run(debug = True)
