f758bf88e18c71afbd3ba43235234708dae6fbec
Anukul Anand <anukul.anand2000@gmail.com>
2021-05-24 19:36:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask , request
 from flask.json import jsonify
 
 app = Flask(__name__)
-@app.route('/<string:name>', methods=['GET'])
+@app.route('/<string:name>', methods=['POST'])
 def get(name):
     get_val(name)
-    return {'data' : name }
+    return {'data' : 'ANSHU' }
 
 
 
