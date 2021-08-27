94436f0c5045914c20da42516af9c65cd4c91b05
Anukul Anand <anukul.anand2000@gmail.com>
2021-08-28 03:15:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,10 @@ from flask import Flask , request
 from flask.json import jsonify
 
 app = Flask(__name__)
-@app.route('/<string:name>', methods=['POST'])
-def get(name):
-    get_val(name)
-    return {'data' : 'ANSHU' }
+@app.route('/veg', methods=['GET'])
+def get():
+    return "Sreeniketh Jod"
 
-@app.route('/')
-def login():
-    return render_template("index.html")
-
-
-def  get_val(name):
-    val =name
-    print(val)
-    return None 
-    
-
-
-
-
-    
 
 if __name__ == '__main__':
    app.run(debug=True)
