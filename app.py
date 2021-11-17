014ff7a8bbefeb89ef228a5f6a41d76dd3dc9086
Anukul Anand <anukul.anand2000@gmail.com>
2021-11-17 22:35:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask , request
+import json
+from flask import Flask, request ,url_for
 from flask.json import jsonify
 
 app = Flask(__name__)
 @app.route('/veg', methods=['GET'])
 def get():
-    return "Sreeniketh Jod"
+    return render_template
+
+
+@app.route('/', methods=['GET'])
+def query_records():
+    name = request.args.get('name')
+    print(name)
+    print(url_for('query_records'))
+
+    return jsonify(name)
+
+response = {
+    'Success' : 'True'
+}
+
+@app.route('/post', methods=['POST'])
+def update_record():
+    record = request.form
+    print(record)
+    print(record['Id'])
+    print(record['Name'])
+    print(record['Value'])
+    print(url_for('update_record'))
+
+    return  record#jsonify(record)
 
 
 if __name__ == '__main__':
